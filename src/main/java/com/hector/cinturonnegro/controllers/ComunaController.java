c3d1de95b623f2aff24cd40d824726882a60e3d9
TorresFrancisco <96franciscotorres@gmail.com>
2021-07-22 17:36:49 -0400

    Arregle el cagaso cabres

--- src/main/java/com/hector/cinturonnegro/controllers/ComunaController.java
+++ src/main/java/com/hector/cinturonnegro/controllers/ComunaController.java
@@ -1,12 +1,9 @@
 package com.hector.cinturonnegro.controllers;
 
-import com.hector.cinturonnegro.models.Comuna;
+
 import com.hector.cinturonnegro.services.ComunaService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 
 @Controller
 public class ComunaController {
@@ -14,118 +11,6 @@ public class ComunaController {
 
     public ComunaController(ComunaService comunaService){
         this.comunaService = comunaService;
-    }
-
-    @GetMapping("/metropolitana")
-    public String indexMetropolitana(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Metropolitana");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/arica")
-    public String indexAP(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Arica y Parinacota");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/tarapaca")
-    public String indexTarapaca(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Tarapaca");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/antofagasta")
-    public String indexAntofagasta(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Antofagasta");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/atacama")
-    public String indexAtacama(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Atacama");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/coquimbo")
-    public String indexCoquimbo(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Coquimbo");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/valparaiso")
-    public String indexValparaiso(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Valparaiso");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/ohiggins")
-    public String indexOhiggins(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("O'Higgins");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/maule")
-    public String indexMaule(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Maule");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/nuble")
-    public String indexNuble(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Ñuble");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/biobio")
-    public String indexBiobio(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Biobio");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/araucania")
-    public String indexAraucania(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Araucania");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/rios")
-    public String indexRios(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Rios");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/lagos")
-    public String indexLagos(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Lagos");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/aysen")
-    public String indexAysen(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Aysen");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
-    }
-
-    @GetMapping("/magallanes")
-    public String indexMagallanes(Model model) {
-        List<Comuna> comunas = comunaService.comunaList("Magallanes");
-        model.addAttribute("comunas",comunas);
-        return "/home.jsp";
     }
 
 }
--- src/main/java/com/hector/cinturonnegro/controllers/RegionController.java
+++ src/main/java/com/hector/cinturonnegro/controllers/RegionController.java
@@ -5,7 +5,6 @@ import com.hector.cinturonnegro.services.RegionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,12 +15,117 @@ public class RegionController {
     public RegionController(RegionService regionService){
         this.regionService = regionService;
     }
-    @GetMapping("/regiones/{idRegion}/search/")
-    public String regionesSearch(
-            @RequestParam("search")String regions, Model model){
-        List<Region> Regiones = regionService.regionList(regions);
-        model.addAttribute("regions", regions);
-        return"regiones.jsp";
+
+    @GetMapping("/metropolitana")
+    public String indexMetropolitana(Model model) {
+        List<Region> regiones = regionService.regionList("Metropolitana");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/arica")
+    public String indexAP(Model model) {
+        List<Region> regiones = regionService.regionList("Arica y Parinacota");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/tarapaca")
+    public String indexTarapaca(Model model) {
+        List<Region> regiones = regionService.regionList("Tarapaca");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/antofagasta")
+    public String indexAntofagasta(Model model) {
+        List<Region> regiones = regionService.regionList("Antofagasta");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/atacama")
+    public String indexAtacama(Model model) {
+        List<Region> regiones = regionService.regionList("Atacama");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/coquimbo")
+    public String indexCoquimbo(Model model) {
+        List<Region> regiones = regionService.regionList("Coquimbo");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/valparaiso")
+    public String indexValparaiso(Model model) {
+        List<Region> regiones = regionService.regionList("Valparaiso");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/ohiggins")
+    public String indexOhiggins(Model model) {
+        List<Region> regiones = regionService.regionList("O'Higgins");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/maule")
+    public String indexMaule(Model model) {
+        List<Region> regiones = regionService.regionList("Maule");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/nuble")
+    public String indexNuble(Model model) {
+        List<Region> regiones = regionService.regionList("Ñuble");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/biobio")
+    public String indexBiobio(Model model) {
+        List<Region> regiones = regionService.regionList("Biobio");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/araucania")
+    public String indexAraucania(Model model) {
+        List<Region> regiones = regionService.regionList("Araucania");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/rios")
+    public String indexRios(Model model) {
+        List<Region> regiones = regionService.regionList("Rios");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/lagos")
+    public String indexLagos(Model model) {
+        List<Region> regiones = regionService.regionList("Lagos");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/aysen")
+    public String indexAysen(Model model) {
+        List<Region> regiones = regionService.regionList("Aysen");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
+    }
+
+    @GetMapping("/magallanes")
+    public String indexMagallanes(Model model) {
+        List<Region> regiones = regionService.regionList("Magallanes");
+        model.addAttribute("regiones",regiones);
+        return "/home.jsp";
     }
 
 
