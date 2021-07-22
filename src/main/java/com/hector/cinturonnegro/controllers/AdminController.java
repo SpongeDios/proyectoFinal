989eacb503a24063be09b4cfd8351cc369e4fc93
Hector Alvarado <hectoralvaradocampos83@gmail.com>
2021-07-22 19:42:39 -0400

    Admin de pana
    Admin de pana

--- src/main/java/com/hector/cinturonnegro/controllers/AdminController.java
+++ src/main/java/com/hector/cinturonnegro/controllers/AdminController.java
@@ -1,10 +1,15 @@
 package com.hector.cinturonnegro.controllers;
 
+import com.hector.cinturonnegro.models.Feedback;
+import com.hector.cinturonnegro.models.Publication;
 import com.hector.cinturonnegro.models.User;
+import com.hector.cinturonnegro.services.FeedbackService;
+import com.hector.cinturonnegro.services.PublicationService;
 import com.hector.cinturonnegro.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -12,11 +17,15 @@ import java.util.List;
 @Controller
 public class AdminController {
     private final UserService userService;
+    private final PublicationService publicationService;
+    private final FeedbackService feedbackService;
 
     //admin == userRol => 3
 
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, PublicationService publicationService, FeedbackService feedbackService) {
         this.userService = userService;
+        this.publicationService = publicationService;
+        this.feedbackService = feedbackService;
     }
 
     @GetMapping("/admin")
@@ -29,10 +38,86 @@ public class AdminController {
         if(user.getRol() != 3){
             return "redirect:/publicaciones";
         }else{
-            List<User> allUser = userService.allData();
-            model.addAttribute("allUser", allUser);
             return "admin.jsp";
         }
+    }
+
+    @GetMapping("/admin/allusers")
+    public String adminAllUsers(HttpSession session, Model model){
+        if(session.getAttribute("userid") == null){
+            return "redirect:/";
+        }
+        Long idUser = (Long) session.getAttribute("userid");
+        User user = userService.findById(idUser);
+        if(user.getRol() != 3){
+            return "redirect:/publicaciones";
+        }else{
+            List<User> users = userService.allData();
+            model.addAttribute("data", users);
+            return "adminData.jsp";
+        }
+    }
+
+    @GetMapping("/admin/allpublications")
+    public String adminAllPublications(HttpSession session, Model model){
+        if(session.getAttribute("userid") == null){
+            return "redirect:/";
+        }
+        Long idUser = (Long) session.getAttribute("userid");
+        User user = userService.findById(idUser);
+        if(user.getRol() != 3){
+            return "redirect:/publicaciones";
+        }else{
+            List<Publication> publications = publicationService.allData();
+            model.addAttribute("data", publications);
+            return "adminData.jsp";
+        }
+    }
+
+
+    @GetMapping("/admin/{idUsuario}/createAdmin")
+    public String addAdmin(
+            HttpSession session,
+            @PathVariable("idUsuario") Long idUsuario
+    ){
+        if(session.getAttribute("userid") == null){
+            return "redirect:/";
+        }
+        Long idUser = (Long) session.getAttribute("userid");
+        User user = userService.findById(idUser);
+        if(user.getRol() != 3){
+            return "redirect:/publicaciones";
+        }else{
+            User adminNew = userService.findById(idUsuario);
+            adminNew.setRol(3);
+            userService.update(adminNew);
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/admin/{idPublicacion}/borrarpublicacion")
+    public String deletePublicacion(
+            HttpSession session,
+            @PathVariable("idPulicacion") Long idPublicacion
+    ){
+        if(session.getAttribute("userid") == null){
+            return "redirect:/";
+        }
+        Long idUser = (Long) session.getAttribute("userid");
+        User user = userService.findById(idUser);
+        if(user.getRol() != 3){
+            return "redirect:/publicaciones";
+        }else{
+            Publication publication = publicationService.findById(idPublicacion);
+            publication.setCategory(null);
+            publication.setUser(null);
+            for (Feedback feedback : publication.getFeedback()) {
+                feedbackService.delete(feedback.getId());
+            }
+            publicationService.delete(publication.getId());
+            return "redirect:/admin";
+        }
+
     }
 
 }
---
+++ src/main/webapp/WEB-INF/adminData.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Hector
+  Date: 22-07-2021
+  Time: 18:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<c:out value="${data}"/>
+</body>
+</html>
